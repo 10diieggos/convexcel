6a2970faaab3bda902cafc7f65116130860bf5b7
Diego Richard <10diieggos@gmail.com>
2020-08-22 05:45:02 -0300

    Aula 178 - Escrita de arquivos (modulo fs)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const fs = require('fs'); //modulo para a manipulação de arquivos pelo node
 
-fs.readFile('./diego.richard',{encoding: 'utf-8'}, (error, content) => { 
+
+fs.writeFile('./diego.richard', 'This is a new content added by fs.writeFile() method', (error) => {
+  if (error) {
+    console.log('Failed to add content');
+  }
+});
+
+fs.readFile('./diego.richard', { encoding: 'utf-8' }, (error, content) => { 
   if (error) {
     console.log('Failed to read the file');
   } else {
     console.log(content);
   }
 });
-
